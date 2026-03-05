b6a400f657ed8a900ac7cd19a19a8d192bf67175
desarrollo-movil-idat <dam1.idat@gmail.com>
2026-03-05 10:18:34 -0500

    ejercicio terminado

--- app/src/main/java/pe/edu/idat/app_recyclerview/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/app_recyclerview/MainActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import pe.edu.idat.app_recyclerview.adapter.PersonajeAdapter
+import pe.edu.idat.app_recyclerview.databinding.ActivityMainBinding
+import pe.edu.idat.app_recyclerview.model.Personaje
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.rvpersonaje.layoutManager = LinearLayoutManager(this)
+        binding.rvpersonaje.adapter = PersonajeAdapter(getPersonajes())
+    }
+    fun getPersonajes(): List<Personaje> {
+        return listOf(
+            Personaje(1, "Julio Mendez", "Jefe", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"),
+            Personaje(2, "Carlos Mendez", "Asistente", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"),
+            Personaje(3, "Piero Mendez", "Practicante", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"),
+            Personaje(4, "Wilfredo Mendez", "Analista", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"),
+            Personaje(5, "Julia Mendez", "Subgerente", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"),
+            Personaje(6, "Marco Mendez", "Gerente", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"),
+            Personaje(7, "Karla Mendez", "Lider técnico", "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U")
+        )
     }
 }
