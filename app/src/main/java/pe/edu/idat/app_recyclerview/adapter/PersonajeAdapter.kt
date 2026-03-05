baa67e4509ae31c8d69a3c78251317712f382045
desarrollo-movil-idat <dam1.idat@gmail.com>
2026-03-05 09:03:04 -0500

    adapter terminado

--- app/src/main/java/pe/edu/idat/app_recyclerview/adapter/PersonajeAdapter.kt
+++ app/src/main/java/pe/edu/idat/app_recyclerview/adapter/PersonajeAdapter.kt
@@ -1,4 +1,40 @@
 package pe.edu.idat.app_recyclerview.adapter
 
-class PersonajeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import pe.edu.idat.app_recyclerview.databinding.ItemPersonajeBinding
+import pe.edu.idat.app_recyclerview.model.Personaje
+
+class PersonajeAdapter(private var listaPersonaje: List<Personaje>)
+    : RecyclerView.Adapter<PersonajeAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemPersonajeBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PersonajeAdapter.ViewHolder {
+        val binding = ItemPersonajeBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PersonajeAdapter.ViewHolder, position: Int) {
+        with(holder){
+            with(listaPersonaje[position]){
+                binding.tvnombres.text = nombres
+                binding.tvcargo.text = cargo
+                Glide.with(itemView.context)
+                    .load(urlimagen)
+                    .into(binding.ivpersonaje)
+            }
+        }
+    }
+
+    override fun getItemCount() = listaPersonaje.size
+
+
 }
---
+++ app/src/main/java/pe/edu/idat/app_recyclerview/model/Personaje.kt
@@ -0,0 +1,8 @@
+package pe.edu.idat.app_recyclerview.model
+
+data class Personaje(
+    var id: Int,
+    var nombres: String,
+    var cargo: String,
+    var urlimagen: String
+)
